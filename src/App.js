c2cf9c7ba066ac78691bc3ca38d51ad56c908d31
farhanbinamjad <farhanbinamjad@gmail.com>
2021-05-03 17:31:13 -0400

    added rest of components (excuding styling)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Summary from './components/Summary';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Header/>
+      <About/>
+      <Summary/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
